2cdd2131d06191b1b452d7e698c2c7fbb25f48f0
JCM <jcl.maquinay@gmail.com>
2024-11-05 16:45:17 +0100

    implement material dashboard app

--- apps/frontend/material/dashboard-material-layout/src/app/app.routes.ts
+++ apps/frontend/material/dashboard-material-layout/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Route } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './page/home/home.component';
+import { SalesComponent } from './page/sales/sales.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'sales',
+        component: SalesComponent
+      }
+    ]
+  }
+];
---
+++ apps/frontend/material/dashboard-material-layout/src/app/layout/pageheader/pageheader.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pageheader',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pageheader.component.html',
+  styleUrl: './pageheader.component.css',
+})
+export class PageheaderComponent {}
