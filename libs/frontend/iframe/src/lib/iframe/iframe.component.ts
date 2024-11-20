6c586b83072b69d09d16f292e14704b458635008
JCM <jcl.maquinay@gmail.com>
2024-11-20 17:27:19 +0100

    Migrate control flow

--- libs/frontend/iframe/src/lib/iframe/iframe.component.ts
+++ libs/frontend/iframe/src/lib/iframe/iframe.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+
 import { Component } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { SafePipe } from './safe.pipe';
@@ -8,9 +8,8 @@ import { SafePipe } from './safe.pipe';
   selector: 'lib-iframe',
   standalone: true,
   imports: [
-    CommonModule,
     SafePipe
-  ],
+],
   templateUrl: './iframe.component.html',
   styleUrl: './iframe.component.scss',
 })
