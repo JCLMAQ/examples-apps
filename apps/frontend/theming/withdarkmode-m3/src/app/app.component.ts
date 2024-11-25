b6bd4c1038035ac97de5353d13be49ba33a6de80
JCM <jcl.maquinay@gmail.com>
2024-11-25 15:14:23 +0100

    Add withdarkm3

---
+++ apps/frontend/theming/withdarkmode-m3/src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { Component, HostBinding, OnInit, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterOutlet } from '@angular/router';
+import { Observable, map, shareReplay } from 'rxjs';
+
+@Component({
+    selector: 'app-root',
+    imports: [
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatListModule,
+        MatIconModule,
+        MatButtonModule,
+        AsyncPipe,
+        RouterOutlet,
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  @HostBinding('class')
+  currentTheme: 'light-theme' | 'dark-theme' = 'light-theme';
+  isDarkMode = false;
+
+  ngOnInit(): void {
+    this.currentTheme =
+      localStorage.getItem('theme') == 'dark-theme'
+        ? 'dark-theme'
+        : 'light-theme';
+    this.isDarkMode = this.currentTheme == 'dark-theme';
+  }
+
+  onThemeChanged() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      this.currentTheme = 'dark-theme';
+    } else {
+      this.currentTheme = 'light-theme';
+    }
+    localStorage.setItem('theme', this.currentTheme);
+  }
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+}
---
+++ apps/frontend/theming/withdarkmode-m3/src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+
+@Component({
+    selector: 'app-home',
+    imports: [MatCardModule, MatButtonModule, MatChipsModule],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
+})
+export class HomeComponent {}
